657e173cdb107c532da1ab2eed01eb1daeb03709
nexar1024 <nexar.futbol.6@gmail.com>
2023-04-18 00:08:59 -0500

    commit

--- ajax_encuesta/mostrarEncuestas.php
+++ ajax_encuesta/mostrarEncuestas.php
@@ -8,41 +8,34 @@ $data = '
     <table class="table table-bordered table-hover table-condensed">
         <thead class="thead-dark">
             <tr>
-                <th>ID encuesta</th>
-                <th>Título</th>
-                <th width="100">Descripción</th>
-                <th>Estado</th>
-                <th>Fecha Inicio</th>
-                <th>Fecha Final</th>
+                <th>ID usuario</th>
+                <th>Nombres</th>
+                <th>Apellidos</th>
+                <th>Email</th>
+                <th>Clave</th>
                 <th>Accciones</th>
             </tr>
         </thead>';
 
-$query = "SELECT * FROM encuestas ORDER BY id_encuesta DESC";
+$query = "SELECT * FROM usuarios ORDER BY id_usuario DESC";
 $resultado = $con->query($query);
 
 while ($row = $resultado->fetch_assoc()) {
     $data .= '
         <tbody>
             <tr>
-                <td>' . $row["id_encuesta"] . '</td>
-                <td><a href="mostrarpreguntas.php?id_encuesta=' . $row['id_encuesta'] . '">' . $row['titulo'] . '</a></td>
-                <td width="100">' . mb_strimwidth($row["descripcion"], 0, 30, "...") . '</td>
-                <td>' . $row["estado"] . '</td>
-                <td>' . $row["fecha_inicio"] . '</td>
-                <td>' . $row["fecha_final"] . '</td>
+                <td>' . $row["id_usuario"] . '</td>
+                <td>' . $row['nombres'] . '</td>
+                <td>' . $row["apellidos"]. '</td>
+                <td>' . $row["email"] . '</td>
+                <td>' . $row["clave"] . '</td>
                 <td>
                     <button id="btnGroupDrop1" type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                       Accciones
                     </button>
                     <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-                        <button onclick="obtenerDetallesEncuesta(' . $row['id_encuesta'] . ')" class="dropdown-item btn btn-warning">Modificar</button>
-                        <button onclick="eliminarEncuesta(' . $row['id_encuesta'] . ')" class="dropdown-item btn btn-danger">Eliminar</button>
-                        <button onclick="publicarEncuesta(' . $row['id_encuesta'] . ')" class="dropdown-item btn btn-primary">Publicar</button>
-                        <button onclick="finalizarEncuesta(' . $row['id_encuesta'] . ')" class="dropdown-item btn btn-secondary">Finalizar</button>
-
-                        <a class="dropdown-item btn btn-secondary" href="vistaprevia.php?id_encuesta=' . $row['id_encuesta'] . '">Vista Previa</a>
-                        <a class="dropdown-item btn btn-secondary" href="resultados.php?id_encuesta=' . $row['id_encuesta'] . '">Resultados</a>
+                        <button onclick="obtenerDetallesUsuario(' . $row['id_usuario'] . ')" class="dropdown-item btn btn-warning">Modificar</button>
+                        <button onclick="eliminarUsuario(' . $row['id_usuario'] . ')" class="dropdown-item btn btn-danger">Eliminar</button>
                     </div>
                 </td>
             </tr>
