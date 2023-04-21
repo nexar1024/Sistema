1387b756ca8e58d589aa690ee4d010fb2a0377f2
nexar1024 <nexar.futbol.6@gmail.com>
2023-04-21 00:36:47 -0500

    a

--- app/uploadfile.php
+++ app/uploadfile.php
@@ -3,15 +3,13 @@
   
   	include_once("config.php");
   	  	   date_default_timezone_set('America/Guayaquil');
-       // $id_entrevista = $_POST['id_entrevista'];
-       $id_entrevista = 10;
         $originalImgName= date('dmYHis') ."-". $_FILES['filename']['name'];
         $tempName= $_FILES['filename']['tmp_name'];
         $folder="uploadedFiles/";
         $url = "/uploadedFiles/".$originalImgName; //update path as per your directory structure 
         
         if(move_uploaded_file($tempName,$folder.$originalImgName)){
-                $query = "INSERT INTO  resultadosentrevista(id_entrevista,audio) VALUES ('$id_entrevista', '$url')";
+                $query = "INSERT INTO resultadosentrevista (id_entrevista,audio) VALUES ('10','$url')";
                 if(mysqli_query($con,$query)){
                 
                 	 $query= "SELECT * FROM resultadosentrevista WHERE audio='$url'";
